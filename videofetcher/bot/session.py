08d9b71eb8cb5e329001e55f77e84387aa969a9b
ResupinePuma <cherchip50@gmail.com>
2022-01-19 13:18:58 -0800

    tiktok via proxy

---
+++ videofetcher/bot/session.py
@@ -0,0 +1,70 @@
+import asyncio, aiohttp
+import requests
+import re
+
+class PSession(requests.Session):
+    def __init__(self) -> None:
+        self.results = []
+        self.working_proxy_list = []
+        self.proxy_list = self.get_proxy_list()
+        super().__init__()
+
+    def get_proxy_list(self):
+        plist = requests.get("https://spys.me/proxy.txt")
+        return [p for p in re.findall("\n([0-9.:]*)",plist.text) if p]
+
+    async def __request(self, name, queue):
+        method, url, kwargs = await queue.get()
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.request(method, url, **kwargs) as response:
+                    #print(f"{name} Status:", response.status)               
+                    response.text = await response.text()
+                    response.proxy = kwargs["proxy"]
+                    self.working_proxy_list.append(kwargs["proxy"])
+                    self.results.append(response)
+            except Exception as ex:
+                print(ex)
+                pass
+        queue.task_done()
+
+    def request(self, method, url, **kwargs) -> requests.Response:
+        self.working_proxy_list = []
+        async def req(method, url, **kwargs):
+            queue = asyncio.Queue()
+            for p in self.proxy_list:
+                nwargs = dict(kwargs)
+                nwargs["proxy"] = f"http://{p}"
+                data = (method, url, nwargs)
+                queue.put_nowait(data)
+            tasks = []
+            for i in range(len(self.proxy_list)):
+                task = asyncio.create_task(self.__request(i,queue))
+                tasks.append(task)
+            await queue.join()
+            for task in tasks:
+                task.cancel()
+        asyncio.run(req(method, url, **kwargs))
+        return self.results
+
+
+    def get(self, url, filter=None, **kwargs):
+        ress = self.request("GET", url, **kwargs)
+        if filter:
+            ress = [filter(r) for r in ress if r]
+        if len(ress) > 0:
+            ress = ress[0]
+        return ress
+
+    def post(self, url, filter=None, **kwargs):
+        ress = self.request("POST", url, **kwargs)
+        if filter:
+            ress = [filter(r) for r in ress if r]
+        if len(ress) > 0:
+            ress = ress[0]
+        return ress
+
+if __name__ == "__main__":
+    sess = PSession()
+    resp = sess.get("https://ident.me/", timeout=5)
+    print(resp)
--- videofetcher/bot/tiktok.py
+++ videofetcher/bot/tiktok.py
@@ -3,23 +3,27 @@ from urllib.parse import parse_qsl, urlparse
 import requests
 
 from bot.exceptions import TiktokUrlException
+from bot.session import PSession
+
 
 
 class TikTokDownloader:
     HEADERS = {
-        'Connection': 'keep-alive',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
-        'DNT': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-        'Accept': '*/*',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'no-cors',
-        'Sec-Fetch-Dest': 'video',
-        'Referer': 'https://www.tiktok.com/',
-        'Accept-Language': 'en-US,en;q=0.9,bs;q=0.8,sr;q=0.7,hr;q=0.6',
-        'sec-gpc': '1',
-        'Range': 'bytes=0-',
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "DNT": "1",
+        # "Host": "vm.tiktok.com",
+        "Pragma": "no-cache",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "TE": "trailers",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0",
     }
 
     def __init__(self, url: str, web_id: str):
@@ -29,16 +33,19 @@ class TikTokDownloader:
             'tt_webid_v2': web_id
         }
 
+    def filter(self, response):
+        if response.status <= 200:
+            return response
+
     def get_video_url(self) -> str:
-        session = requests.Session()
-        session.cookies.update(self.__cookies)
-        session.headers = TikTokDownloader.HEADERS
+        session = PSession()
         if len(re.findall(r'(.*tiktok\.com\/@.*\/live)', self.__url)) > 0:
             raise TiktokUrlException("Can't download tiktok live stream. Try another url")
         for _ in range(3):
-            response = session.get(self.__url)
-            matches = re.findall(r'"playAddr":"([a-zA-Z0-9:.\/\\.:&-=?%_]*)"', response.content.decode("utf-8"))
+            response = session.get(self.__url, filter=self.filter, headers=TikTokDownloader.HEADERS, timeout=5)
+            matches = re.findall(r'"playAddr":"([a-zA-Z0-9:.\/\\.:&-=?%_]*)"', response.text)
             if len(matches) > 1 or len(matches) == 0:
                 continue
             return matches[0].replace(r'\u0026', '&').replace(r'\u002F', '/')
         raise TiktokUrlException()
+    
