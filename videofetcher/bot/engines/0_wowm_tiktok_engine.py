f222fe373d46c3e3fc0c2aefb6ed71d0ea03323a
ResupinePuma <cherchip50@gmail.com>
2022-02-25 08:08:39 -0800

    bugfixed

--- videofetcher/bot/engines/0_wowm_tiktok_engine.py
+++ videofetcher/bot/engines/0_wowm_tiktok_engine.py
@@ -45,7 +45,7 @@ class TiktokEngine(AbstractEngine):
 
             self.notifier.update_status(random.choice(PHRASES))
 
-            video_url, video_id = None, None
+            video_links, video_id = None, None
             for i in range(3):
                 self.notifier.make_progress_bar(i*10)
                 response = requests.post(f"{sys_config('SPLASH_URL')}/render.json", json={
@@ -61,7 +61,7 @@ class TiktokEngine(AbstractEngine):
 
                 if "url" in response.json():
                     tt_url = response.json()['url']
-                    matches_id = re.search(r"\/video\/([0-9]*)",tt_url,re.IGNORECASE)
+                    matches_id = re.search(r"\/video|v\/([0-9]*)",tt_url,re.IGNORECASE)
                     if matches_id:
                         video.name = response.json().get('title')
                         video_id = matches_id.group(1)
@@ -75,31 +75,35 @@ class TiktokEngine(AbstractEngine):
             response = requests.post(f"{sys_config('SPLASH_URL')}/render.html", json={
                 "url" : tt_url,
                 "http2" : 1,
-                "wait" : 5,
+                "wait" : 4,
                 "js_source" : 'document.getElementsByClassName("chakra-textarea").item(0).remove();document.getElementsByClassName("css-1cexexp").item(0).remove()',
                 "headers" : [("user-agent", sys_config("USER_AGENT").replace('"',''))],
                 "response_body" : 1
             }, timeout=int(sys_config("PROCESSING_TIMEOUT")))
-            self.notifier.make_progress_bar(50)
+            self.notifier.make_progress_bar(30)
 
-            matches = re.search(r'<a download=\"\" href=\"(.*)\" rel.*Download Video No Watermark \(SD\)', response.text, re.IGNORECASE)
-            if matches:
-                video_url = matches.group(1).split('"')[0]
-                self.notifier.make_progress_bar(80)
+            matches = re.search(r'<a download=\"\" href=\"(.*)\" rel.*<\/a>', response.text, re.IGNORECASE)
+            if matches:                
+                video_links = re.findall(r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})", matches.group(1))            
+                video_links = [l.replace('"',"") for l in video_links if "dl.godownloader.com" in l or "tiktokcdn.com" in l]                
+                self.notifier.make_progress_bar(40)
             else:
                 raise exceptions.DownloadError("Can't download from url. Try again later")
             
-            if not video_url:
+            if not video_links:
                 raise exceptions.DownloadError("Can't download video from this url")
             self.notifier.update_status("⏬ Downloading video")
 
-            response = self.session.get(video_url, timeout=10)            
-            if response.status_code < 400 and response.content:
-                path = self.get_downloaded_file_abspath(name_hash)
-                with open(path, "wb") as f:
-                    f.write(response.content)
-                video.path = path
-                video.size = os.path.getsize(path)
+            for i,video_url in enumerate(video_links):
+                response = self.session.get(video_url, timeout=10)
+                self.notifier.make_progress_bar(50+i*10)        
+                if response.status_code < 400 and response.content:
+                    path = self.get_downloaded_file_abspath(name_hash)
+                    with open(path, "wb") as f:
+                        f.write(response.content)
+                    video.path = path
+                    video.size = os.path.getsize(path)
+                    break
             
             self.notifier.make_progress_bar(100)
         except Exception as ex:
